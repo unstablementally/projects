85b519ee60298ec291232df1c514c3e4f2544393
unstablementally <166523326+unstablementally@users.noreply.github.com>
2024-04-10 01:28:03 +0800

    readme stuff to make it cleaner

--- math/Sec2/Quadratic formula/quadratic_general_formula.py
+++ math/Sec2/Quadratic formula/quadratic_general_formula.py
@@ -1,10 +1,3 @@
-#Note to self: this is parabola also its to get minimum or maximum and the two x intercepts
-#c = y intercept
-#coords of vertex = h = -(b/2a) where h is x coord then since x coords is h then k = (ah^2 + bh + c) where k = y coords
-#vertex = minimum/maximum point or turning/stationary point
-#x intercepts = (-b +-(sqrt(b^2-4ac)))/2a
-#so for eg. x^2-25=0 a=1, b=0, c=-25 (-0 +-(sqrt(100)))/2 means x1 is 5 and x2 is -5
-#done with the help of Microsoft Copilot teaching me the graph
 import math
 a = int(input("What is your a for ax²"))
 b = int(input("What is your b for bx"))
